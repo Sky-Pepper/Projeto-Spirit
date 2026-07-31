c6a8202ed7b3b71c42fe53d8348f453711fcf8d2
Sky-Pepper <tutuhcosta01@gmail.com>
2026-07-30 22:34:33 -0300

    Add files via upload

---
+++ Projeto-segurança/Analisador_senha.py
@@ -0,0 +1,55 @@
+senha = input("Digite sua senha: ")
+
+tem_maiuscula = False
+tem_minuscula = False
+tem_numero = False
+tem_simbolo = False
+
+simbolos = "!@#$%&*?"
+
+for caractere in senha:
+
+    if caractere.isupper():
+        tem_maiuscula = True
+
+    if caractere.islower():
+        tem_minuscula = True
+
+    if caractere.isdigit():
+        tem_numero = True
+
+    if caractere in simbolos:
+        tem_simbolo = True
+
+qualidades = []
+
+if len(senha) >= 8:
+    qualidades.append("✓ Tamanho adequado")
+
+if tem_maiuscula:
+    qualidades.append("✓ Possui letra maiúscula")
+
+if tem_minuscula:
+    qualidades.append("✓ Possui letra minúscula")
+
+if tem_numero:
+    qualidades.append("✓ Possui número")
+
+if tem_simbolo:
+    qualidades.append("✓ Possui símbolo")
+
+print("\nQualidades da senha: ")
+
+for qualidade in qualidades:
+    print(qualidade)
+
+pontos = len(qualidades)
+
+if pontos == 5:
+    print("\nSua senha é: forte")
+
+elif pontos >= 3:
+    print("\nSua senha é: mediana")
+
+else:
+    print("\nSua senha é: fraca")
